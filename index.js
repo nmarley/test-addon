e4a9bf3f5629e7de1e889aac626ba4aa244fee0b
Nathan Marley <nathan.marley@gmail.com>
2019-02-19 17:21:05 -0300

    derived instance

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ console.log('addon', testAddon);
 console.log('hello', testAddon.hello());
 console.log('add', testAddon.add(9, 5));
 
-
 const classInstance = new testAddon.ClassExample(4.3);
 console.log('Testing class initial value : ', classInstance.getValue());
 console.log('After adding 3.3 : ', classInstance.add(3.3));
+
+const newFromExisting = new testAddon.ClassExample(classInstance);
+console.log('Testing class initial value from derived instance: ', newFromExisting.getValue());
 
 module.exports = testAddon;
